0f637d6d4ea6deddc82fd1193095d888e8f56c20
Gyula Szucs <gyula@taotesting.com>
2017-10-05 14:23:18 +0200

    Setup extension

---
+++ manifest.php
@@ -0,0 +1,57 @@
+<?php
+/**  
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ * Copyright (c) 2017 (original work) Open Assessment Technologies SA;
+ *               
+ * 
+ */
+
+/**
+ * Generated using taoDevTools 3.1.1
+ */
+return array(
+    'name' => 'taoTaskQueue',
+    'label' => 'TAO specific Task Queue with custom GUI',
+    'description' => '',
+    'license' => 'GPL-2.0',
+    'version' => '0.1.0',
+    'author' => 'Open Assessment Technologies SA',
+    'requires' => array(
+        'generis' => '>=4.4.3',
+        'tao' => '>=13.1.2'
+    ),
+    'managementRole' => 'http://www.tao.lu/Ontologies/generis.rdf#taoTaskQueueManager',
+    'acl' => array(
+        array('grant', 'http://www.tao.lu/Ontologies/generis.rdf#taoTaskQueueManager', array('ext'=>'taoTaskQueue')),
+    ),
+    'install' => array(
+    ),
+    'uninstall' => array(
+    ),
+    'update' => oat\taoTaskQueue\scripts\update\Updater::class,
+    'routes' => array(
+        '/taoTaskQueue' => 'oat\\taoTaskQueue\\controller'
+    ),    
+    'constants' => array(
+        # views directory
+        "DIR_VIEWS" => dirname(__FILE__).DIRECTORY_SEPARATOR."views".DIRECTORY_SEPARATOR,
+        
+        #BASE URL (usually the domain root)
+        'BASE_URL' => ROOT_URL.'taoTaskQueue/',
+    ),
+    'extra' => array(
+    )
+);
