b06d55d205e3c6b6298d2acdcaf74b7592598b46
Gyula Szucs <gyula@taotesting.com>
2018-01-08 10:58:40 +0100

    TAO-4578 Bump version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -140,6 +140,6 @@ class Updater extends common_ext_ExtensionUpdater
             $this->setVersion('0.13.0');
         }
 
-        $this->skip('0.13.0', '0.13.1');
+        $this->skip('0.13.0', '0.14.0');
     }
 }
