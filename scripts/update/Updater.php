19b9c7f4afbe877418aaa6e5641ded4f6d7ffad1
Pavel Handleman <pavel.handleman@gmail.com>
2019-03-05 16:36:55 +0300

    version bump

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -250,6 +250,6 @@ class Updater extends common_ext_ExtensionUpdater
             $this->setVersion('1.1.0');
         }
 
-        $this->skip('1.1.0', '1.4.0');
+        $this->skip('1.1.0', '1.4.1');
     }
 }
