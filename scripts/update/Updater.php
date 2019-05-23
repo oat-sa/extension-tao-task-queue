3e2e83846e7e263b30083745dd45fe9ee73c1c36
Aleh Hutnikau <hutnikau@1pt.com>
2019-05-23 17:18:39 +0300

    update major version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -250,6 +250,6 @@ class Updater extends common_ext_ExtensionUpdater
             $this->setVersion('1.1.0');
         }
 
-        $this->skip('1.1.0', '2.3.0');
+        $this->skip('1.1.0', '3.0.0');
     }
 }
