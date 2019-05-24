4a13685b80d1419c325efa678a6e5193ec93f2e2
uncleempty <ilya.yarkavets@1pt.com>
2019-05-24 11:41:45 +0300

    Fixed dependency version

--- manifest.php
+++ manifest.php
@@ -35,7 +35,7 @@ return array(
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
         'generis' => '>=5.8.0',
-        'tao' => '>=35.6.0'
+        'tao' => '>=35.7.0'
     ),
     'managementRole' => 'http://www.tao.lu/Ontologies/generis.rdf#taoTaskQueueManager',
     'acl' => array(
