c874f5e2962660bb89ec26b0cf4594f96feffc5a
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2019-05-31 17:48:45 +0200

    migrate controllers to es2015

--- views/js/controller/main.js
+++ views/js/controller/main.js
@@ -13,7 +13,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2017 (original work) Open Assessment Technologies SA ;
+ * Copyright (c) 2017-2019 (original work) Open Assessment Technologies SA ;
  */
 
 /**
@@ -32,56 +32,48 @@ function ($, taskQueueManagerFactory, taskQueue) {
      * @exports taoTaskQueue/controller/main
      */
     return {
-        start: function () {
+        start() {
 
             var taskManager = taskQueueManagerFactory({
-                    replace: true
-                })
-                .on('remove', function (taskId) {
-                    taskQueue.archive(taskId);
-                })
-                .on('report', function (taskId) {
-                    taskQueue.get(taskId).then(function (task) {
-                        //show report in popup
-                        taskManager.showDetail(task);
-                    });
-                })
-                .on('download', function (taskId) {
-                    taskQueue.download(taskId);
-                })
-                .on('redirect', function (taskId) {
-                    taskQueue.redirect(taskId);
-                })
-                .on('listclearfinished', function (){
-                    taskQueue
-                        .pollAllStop()
-                        .archive('all')
-                        .then(function(){
-                            taskQueue.pollAll();
-                        });
-                })
-                .render($('#taskqueue').parent())
-                .hide();//start hidden to prevent blinking effect
+                replace: true
+            })
+            .on('remove', taskId => taskQueue.archive(taskId) )
+            .on('report', taskId => {
+                //show report in popup
+                taskQueue.get(taskId).then( task => taskManager.showDetail(task) );
+            })
+            .on('download', taskId => taskQueue.download(taskId) )
+            .on('redirect', taskId => taskQueue.redirect(taskId) )
+            .on('listclearfinished', () => {
+                taskQueue
+                    .pollAllStop()
+                    .archive('all')
+                    .then( () => taskQueue.pollAll() );
+            })
+            .render($('#taskqueue').parent())
+            .hide();//start hidden to prevent blinking effect
 
             //listen to events started by the task queue model
-            taskQueue.on('taskcreated', function(data){
-                if(taskManager.list.is('hidden')){
-                    taskManager.absorbBurst(data.sourceDom, [0, 300, 600]).then(function(){
-                        taskManager.addNewTask(data.task);
+            taskQueue
+                .on('taskcreated', data => {
+                    if(taskManager.list.is('hidden')){
+                        taskManager.absorbBurst(data.sourceDom, [0, 300, 600]).then( () => {
+                            taskManager.addNewTask(data.task);
+                            taskQueue.pollAll();
+                        });
+                    }else{
+                        taskManager.addNewTask(data.task, true);
                         taskQueue.pollAll();
-                    });
-                }else{
-                    taskManager.addNewTask(data.task, true);
-                    taskQueue.pollAll();
-                }
-            }).on('multitaskstatuschange', function(){
-                taskManager.pulse();
-            }).on('pollAll', function (tasks) {
-                if(taskManager.is('hidden')){
-                    taskManager.show();
-                }
-                taskManager.loadData(tasks);
-            }).pollAll(true);//start polling immediately on load
+                    }
+                })
+                .on('multitaskstatuschange', () =>  taskManager.pulse() )
+                .on('pollAll', tasks => {
+                    if (taskManager.is('hidden')) {
+                        taskManager.show();
+                    }
+                    taskManager.loadData(tasks);
+                })
+                .pollAll(true);//start polling immediately on load
         }
     };
 });
