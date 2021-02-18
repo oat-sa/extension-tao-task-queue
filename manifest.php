1c53bc34d5edfffbffbad863d453712250780193
Aleh Hutnikau <hutnikau@1pt.com>
2021-02-18 12:52:06 +0300

    chore!: move dependencies to composer.json

--- manifest.php
+++ manifest.php
@@ -32,12 +32,7 @@ return [
     'label' => 'Task Queue',
     'description' => 'Extended Task Queue functionalities with custom GUI',
     'license' => 'GPL-2.0',
-    'version' => '5.5.1',
     'author' => 'Open Assessment Technologies SA',
-    'requires' => [
-        'generis' => '>=12.20.2',
-        'tao' => '>=38.9.5'
-    ],
     'managementRole' => 'http://www.tao.lu/Ontologies/generis.rdf#taoTaskQueueManager',
     'acl' => [
         ['grant', 'http://www.tao.lu/Ontologies/generis.rdf#taoTaskQueueManager', ['ext' => 'taoTaskQueue']],
