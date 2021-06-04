f2925b687f6849bae3393ae959a42feff302c113
Gabriel Felipe Soares <gabriel.felipe.soares@taotesting.com>
2021-06-04 17:21:08 +0200

    chore: add service locator

--- model/TaskLog/Decorator/SimpleManagementCollectionDecorator.php
+++ model/TaskLog/Decorator/SimpleManagementCollectionDecorator.php
@@ -22,6 +22,8 @@
 namespace oat\taoTaskQueue\model\TaskLog\Decorator;
 
 use oat\oatbox\filesystem\FileSystemService;
+use oat\oatbox\service\ServiceManager;
+use oat\tao\model\taskQueue\Report\TaskLogTranslator;
 use oat\tao\model\taskQueue\TaskLog\CollectionInterface;
 use oat\tao\model\taskQueue\TaskLogInterface;
 use oat\taoTaskQueue\model\Entity\Decorator\CategoryEntityDecorator;
@@ -73,7 +75,12 @@ class SimpleManagementCollectionDecorator extends TaskLogCollectionDecorator
     {
         $data = [];
 
+        /** @var TaskLogTranslator $translator */
+        $translator = ServiceManager::getServiceManager()->get(TaskLogTranslator::class); //@FIXME @TODO Inject this in the constructor
+
         foreach ($this->getIterator() as $entity) {
+            $entity = $translator->translate($entity);
+
             $entityData = (new HasFileEntityDecorator(new CategoryEntityDecorator($entity, $this->taskLogService), $this->fileSystemService))->toArray();
 
             if (!$this->reportIncluded && array_key_exists('report', $entityData)) {
