6e4d5d27f709d1c6282a92c76efa13187a78e2c9
Gabriel Felipe Soares <gabriel.felipe.soares@taotesting.com>
2021-07-16 17:06:16 +0200

    chore: add unit test

--- test/unit/model/Repository/StuckTaskRepositoryTest.php
+++ test/unit/model/Repository/StuckTaskRepositoryTest.php
@@ -23,15 +23,89 @@ declare(strict_types=1);
 namespace oat\taoTaskQueue\test\unit\model\Repository;
 
 use oat\generis\test\TestCase;
+use oat\tao\model\taskQueue\QueueDispatcherInterface;
+use oat\tao\model\taskQueue\QueueInterface;
+use oat\tao\model\taskQueue\Task\TaskInterface;
+use oat\tao\model\taskQueue\TaskLog;
+use oat\tao\model\taskQueue\TaskLog\Entity\EntityInterface;
+use oat\tao\model\taskQueue\TaskLogInterface;
+use oat\taoTaskQueue\model\QueueBroker\RdsQueueBroker;
+use oat\taoTaskQueue\model\Repository\StuckTaskQuery;
+use oat\taoTaskQueue\model\Repository\StuckTaskRepository;
+use PHPUnit\Framework\MockObject\MockObject;
 
 class StuckTaskRepositoryTest extends TestCase
 {
+    /** @var StuckTaskRepository */
+    private $subject;
+
+    /** @var TaskLogInterface|MockObject */
+    private $taskLog;
+
+    /** @var QueueDispatcherInterface|MockObject */
+    private $queueDispatcher;
+
     public function setUp(): void
     {
+        $this->taskLog = $this->createMock(TaskLogInterface::class);
+        $this->queueDispatcher = $this->createMock(QueueDispatcherInterface::class);
+        $this->subject = new StuckTaskRepository();
+        $this->subject->setServiceLocator(
+            $this->getServiceLocatorMock(
+                [
+                    TaskLogInterface::SERVICE_ID => $this->taskLog,
+                    QueueDispatcherInterface::SERVICE_ID => $this->queueDispatcher,
+                ]
+            )
+        );
     }
 
     public function testFindAll(): void
     {
-        $this->markTestSkipped('TODO');
+        $query = new StuckTaskQuery(
+            'query',
+            [
+                'task2',
+            ],
+            [
+                TaskLog::STATUS_ENQUEUED,
+            ],
+            300
+        );
+
+        $queue = $this->createMock(QueueInterface::class);
+        $broker = $this->createMock(RdsQueueBroker::class);
+        $taskLogEntity = $this->createMock(EntityInterface::class);
+        $taskLogs = [$taskLogEntity];
+
+        $this->taskLog
+            ->expects($this->once())
+            ->method('search')
+            ->willReturn($taskLogs);
+
+        $this->queueDispatcher
+            ->expects($this->once())
+            ->method('getQueue')
+            ->willReturn($queue);
+
+        $queue->expects($this->once())
+            ->method('getBroker')
+            ->willReturn($broker);
+
+        $broker->expects($this->once())
+            ->method('getTaskByTaskLogId')
+            ->willReturn(null);
+
+        $taskLogEntity->expects($this->once())
+            ->method('getCreatedAt')
+            ->willReturn($query->getAgeDateTime());
+
+        $taskLogEntity->expects($this->once())
+            ->method('getId')
+            ->willReturn('id');
+
+        $collection = $this->subject->findAll($query);
+
+        $this->assertCount(1, $collection);
     }
 }
